30fd08029499e82528de62c0dcab7e84f7095a74
Rohit Kumar <nnrohu@gmail.com>
2023-08-08 12:04:29 +0530

    Stage 3/6: Decrypted

--- task/src/Main.kt
+++ task/src/Main.kt
@@ -1,18 +1,28 @@
 package encryptdecrypt
 
 fun main() {
-    //('z' - (ch - 'a'))
-    var str = "abcdefghijklmnopqrstuvwxyz".toCharArray()
-
+    val type = readln()
     val input = readln()
     val key = readln().toInt()
+    when(type){
+        "enc" -> enc(input,key)
+        "dec" -> dec(input,key)
+    }
+}
+
+fun enc(input: String, key: Int) {
     var encrypt = ""
     input.forEach {   ch ->
-        if(str.contains(ch)) {
-          val eIndex =  ( str.indexOf(ch) + key ) % 26
-            encrypt += str[eIndex]
-        }
-       else encrypt+=ch
-     }
+       val eChar = ch.code + key
+        encrypt+= Char(eChar)
+    }
     println(encrypt)
 }
+ fun dec(input: String, key: Int) {
+     var decrypt = ""
+     input.forEach {   ch ->
+         val eChar = ch.code - key
+         decrypt+= Char(eChar)
+     }
+     println(decrypt)
+ }
