1ff0029cece92225fe9cff4c935ee2cea8d49b45
Rohit Kumar <nnrohu@gmail.com>
2023-08-08 12:02:05 +0530

    Stage 2/6: Knowledge is key

--- task/src/Main.kt
+++ task/src/Main.kt
@@ -2,9 +2,17 @@ package encryptdecrypt
 
 fun main() {
     //('z' - (ch - 'a'))
-    var str = "we found a treasure!"
-    str.forEach {   ch ->
-        if(ch == ' ' || ch == '!') print( ch)
-       else  print(('z' - (ch - 'a')))
+    var str = "abcdefghijklmnopqrstuvwxyz".toCharArray()
+
+    val input = readln()
+    val key = readln().toInt()
+    var encrypt = ""
+    input.forEach {   ch ->
+        if(str.contains(ch)) {
+          val eIndex =  ( str.indexOf(ch) + key ) % 26
+            encrypt += str[eIndex]
+        }
+       else encrypt+=ch
      }
+    println(encrypt)
 }
