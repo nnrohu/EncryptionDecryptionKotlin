98ac6bc9f434c9939ff8039f0b944fceb7b72fe9
Rohit Kumar <nnrohu@gmail.com>
2023-08-08 11:59:01 +0530

    Stage-1/6-Encrypted!

--- task/src/Main.kt
+++ task/src/Main.kt
@@ -2,5 +2,9 @@ package encryptdecrypt
 
 fun main() {
     //('z' - (ch - 'a'))
-
+    var str = "we found a treasure!"
+    str.forEach {   ch ->
+        if(ch == ' ' || ch == '!') print( ch)
+       else  print(('z' - (ch - 'a')))
+     }
 }
